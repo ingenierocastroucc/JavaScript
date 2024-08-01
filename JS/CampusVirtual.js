1694dc3160783f418047cd3d9d73bed149cbfedb
Pedro <ingenierocastroucc@gmail.com>
2024-07-31 21:40:59 -0500

    Push inicial del proyecto CampusVirtual.js

---
+++ JS/CampusVirtual.js
@@ -0,0 +1,65 @@
+//Texto de bienvenida
+console.log('!Hola¡ Bienvenido a este tu campus virtual')
+
+//Declaracion de variable
+
+//Tipos de datos primitivos
+
+//Number
+let valorMatricula = 400000
+console.log('Valor de la matricula:' + ' ' + valorMatricula)
+
+//String
+let asignatura = 'Calculo'
+console.log('Asignatura:' + ' ' +asignatura)
+
+//bollean
+let matriculaActiva = true
+console.log('Matricula activa:' + ' ' + matriculaActiva)
+
+//null
+let nivelacion = null
+console.log('Nivelacion:' + ' ' + nivelacion)
+
+//undefined
+let matriculaNivelacion = undefined
+console.log('Nivelacion:' + ' ' + matriculaNivelacion)
+
+//Symbol
+let sexoEstudiante = Symbol('Sexo estudiante: M')
+console.log(sexoEstudiante)
+
+//biginit
+let materiasPensunt = 2n
+console.log('Materias Pensunt:' + ' ' + materiasPensunt)
+
+//Tipos de datos complejos
+
+//Object
+console.log('Objeto matricula:')
+let matricula = 
+{
+    IdMatricula: 1,
+    Asignatrua: 'Calculo',
+    ValorMatricula: 400000,
+    ProfesorAsignatura: 'Pedro Diaz',
+    AsignaturaId: 1
+}
+console.log(matricula)
+
+//Array
+console.log('Arrays:')
+let asignaturas = 
+[
+    'Calculo',
+    'C#',
+    'SQL'
+]
+console.log(asignaturas)
+
+//Function
+function asignaturasMatriculadas ()
+{
+    
+}
+
